94f119becf86de41ef2eaa4d23cb4cf08faea9e1
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-07-08 15:52:02 +0700

    a
    a

--- application/views/mebel/login.php
+++ application/views/mebel/login.php
@@ -47,16 +47,16 @@
                                 <div class="col-md-12 form-group p_star">
                                     <input type="password" class="form-control" id="password" name="password" placeholder="Password">
                                 </div>
-                                
+
                                 <div class="col-md-12 form-group">
                                     <div class="creat_account d-flex align-items-center">
-                                        <input type="checkbox" id="f-option" required name="selector">
-                                        <label for="f-option">Remember me</label>
+                                        <input type="checkbox" id="show-password" onclick="myFunction()" name="selector">
+                                        <label for="f-option">Show Password</label>
                                     </div>
                                     <button type="submit" class="btn_3">
                                         log in
                                     </button>
-                                    <a class="lost_pass" href="#">forget password?</a>
+                                    <a class="lost_pass" href="#" data-toggle="modal" data-target="#newMenuModal">forget password?</a>
                                 </div>
                                 <a class="lost_pass" href="<?php echo base_url('Owner_controller/A_login') ?>">Anda Admin?, klik disini</a>
                             </form>
@@ -107,7 +107,62 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="newMenuModal" tabindex="-1" role="dialog" aria-labelledby="newMenuModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newMenuModalLabel">Perbarui Passwword</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?php echo base_url('Landing_controller/Login/ubahpassword') ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="nama_kostumer" name="nama_kostumer" placeholder="Nama">
+					</div>
+					<br>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="email" name="email" placeholder="Email">
+					</div>
+					<br>
+					<div class="form-group">
+                        <input type="text" class="form-control" id="no_telp" name="no_telp" placeholder="Telp">
+					</div>
+					<br>
+					<div class="form-group">
+                        <input type="password" class="form-control" id="password1" name="password1" placeholder="Password Baru">
+					</div>
+					<br>
+					<div class="form-group">
+                        <input type="password" class="form-control" id="password2" name="password2" placeholder="Konfirmasi Password">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+                    <button type="submit" class="btn btn-success">Ubah Password</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div> 
     <!--================login_part end =================-->
+
+    <script type="text/javascript">
+        function myFunction() {
+            // Get the checkbox
+            var checkBox = document.getElementById("show-password");
+            // Get the output text
+            var password = document.getElementById("password");
+
+            // If the checkbox is checked, display the output text
+            if (checkBox.checked == true) {
+                password.type = "text";
+            } else {
+                password.type = "password";
+            }
+        }
+    </script>
     <!--::footer_part start::-->
     <?php $this->load->view('template/footer'); ?>
     <!--::footer_part end::-->
