534c3c84fb58c49d6db8e8cc2bbf71b5455c9b59
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-07-19 23:34:57 +0700

    a
    a

--- application/views/mebel/coba.php
+++ application/views/mebel/coba.php
@@ -1,8 +1,8 @@
 <?php 
 $id = 'PR0001';
-$panjang = 0;
-$lebar = 0;
-$tinggi = 1;
+$panjang = 1;
+$lebar = -1;
+$tinggi = -1;
 $harga = 100000;
 $produk = $this->db->query("SELECT * FROM produk WHERE id_produk='$id'")->result();
 foreach($produk as $g) {
@@ -10,7 +10,7 @@ foreach($produk as $g) {
     $lebar2 = $g->lebar;
     $tinggi3 = $g->tinggi;
 }
-if($panjang == 0 && $lebar != 0 && $tinggi != 0) {
+if($panjang == 0 && $lebar > 0 && $tinggi > 0) {
     $pjg = $panjang1;
     $lbr = $lebar2+$lebar;
     $tng = $tinggi3+$tinggi;
@@ -18,7 +18,7 @@ if($panjang == 0 && $lebar != 0 && $tinggi != 0) {
     $tambahan2 = $lebar*20000;
     $tambahan3 = $tinggi*20000;
     $hargafix = $harga+$tambahan2+$tambahan3;
-}else if($lebar == 0 && $panjang != 0 && $tinggi != 0) {
+}else if($lebar == 0 && $panjang > 0 && $tinggi > 0) {
     $pjg = $panjang1+$panjang;
     $lbr = $lebar2;
     $tng = $tinggi3+$tinggi3;
@@ -26,7 +26,7 @@ if($panjang == 0 && $lebar != 0 && $tinggi != 0) {
     $tambahan1 = $panjang*20000;
     $tambahan3 = $tinggi*20000;
     $hargafix = $harga+$tambahan1+$tambahan3;
-}else if($tinggi == 0 && $lebar != 0 && $panjang != 0) {
+}else if($tinggi == 0 && $lebar > 0 && $panjang > 0) {
     $pjg = $panjang1+$panjang;
     $lbr = $lebar2+$lebar;
     $tng = $tinggi3;
@@ -38,21 +38,21 @@ if($panjang == 0 && $lebar != 0 && $tinggi != 0) {
     $lbr = $lebar2;
     $tng = $tinggi3;
     $hargafix = $harga;
-}else if($panjang == 0 && $lebar == 0 && $tinggi != 0){
+}else if($panjang == 0 && $lebar == 0 && $tinggi > 0){
     $pjg = $panjang1;
     $lbr = $lebar2;
     $tng = $tinggi3+$tinggi;
     
     $tambahan3 = $tinggi*20000;
     $hargafix = $harga+$tambahan3;
-}else if($panjang == 0 && $lebar != 0 && $tinggi == 0){
+}else if($panjang == 0 && $lebar > 0 && $tinggi == 0){
     $pjg = $panjang1;
     $lbr = $lebar2+$lebar;
     $tng = $tinggi3;
     $tambahan2 = $lebar*20000;
     $tng = $tinggi3;
     $hargafix = $harga+$tambahan2;
-}else if($panjang != 0 && $lebar == 0 && $tinggi == 0){
+}else if($panjang > 0 && $lebar == 0 && $tinggi == 0){
     $pjg = $panjang1+$panjang;
     $lbr = $lebar2;
     $tng = $tinggi3;
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -106,7 +106,6 @@
                                 </a></i>
                         </div>
                     </div>
-
                 </nav>
             </div>
         </div>
